23860d4f3533af7772aea5f302a7db5372324ec5
Mike Shvartsman <ms44@princeton.edu>
2015-07-28 20:05:38 -0400

    Rewrote error checks to be more informative

--- R/wienerdist.R
+++ R/wienerdist.R
@@ -1,21 +1,12 @@
 check_wiener_pars <- function(alpha,tau,beta,delta)
 {
-  if(!is.numeric(alpha) || !is.numeric(tau) || 
-     !is.numeric(beta) || !is.numeric(delta)) {
-    return(FALSE)
-  }
-  if(alpha > 0 & 
-     tau > 0 &
-     beta >= 0 & beta <= 1) return(TRUE)
-  else return(FALSE)
+  stopifnot(is.numeric(alpha) , is.numeric(tau), is.numeric(beta), is.numeric(delta) , alpha > 0 , tau > 0 , beta >= 0 , beta <= 1) 
 }
 
 dwiener <- function(q, alpha,tau,beta,delta, resp="upper", give_log=FALSE) 
 {
-  if (!check_wiener_pars(alpha,tau,beta,delta) ||
-      !is.numeric(q) || !(is.character(resp) || is.factor(resp))) {
-    stop("bad parameter values!")
-  }
+  check_wiener_pars(alpha,tau,beta,delta) 
+  stopifnot(is.numeric(q) && (is.character(resp) || is.factor(resp)))
 
   if (!(length(resp) == length(q))) {
     stop("argument q and resp need to be of the same length!")
@@ -29,26 +20,24 @@ dwiener <- function(q, alpha,tau,beta,delta, resp="upper", give_log=FALSE)
   for (i in 1:length(q)) {
     if (q[i]<0) stop("q must be > 0!")
 
-    if (resp[i] == "upper") 
-      d[i] <- .Call(dwiener_c, q[i], alpha,tau,beta,delta, give_log)
-    else if (resp[i] == "lower") 
-      d[i] <- .Call(dwiener_c, -q[i], alpha,tau,beta,delta, give_log)
-    else if (resp[i] == "both") 
-      d[i] <- .Call(dwiener_c, q[i], alpha,tau,beta,delta, give_log) +
-           .Call(dwiener_c, -q[i], alpha,tau,beta,delta, give_log)
-    else stop("resp must be either 'lower', 'upper' or 'both'")
-    if(is.nan(d[i])) d[i] <- 0
-  }
+      if (resp[i] == "upper") 
+        d[i] <- .Call(dwiener_c, q[i], alpha,tau,beta,delta, give_log)
+      else if (resp[i] == "lower") 
+        d[i] <- .Call(dwiener_c, -q[i], alpha,tau,beta,delta, give_log)
+      else if (resp[i] == "both") 
+        d[i] <- .Call(dwiener_c, q[i], alpha,tau,beta,delta, give_log) +
+      .Call(dwiener_c, -q[i], alpha,tau,beta,delta, give_log)
+      else stop("resp must be either 'lower', 'upper' or 'both'")
+      if(is.nan(d[i])) d[i] <- 0
+    }
 
-  return(d)
-}
+    return(d)
+  }
 
 pwiener <- function(q, alpha,tau,beta,delta, resp="upper")
 {
-  if (!check_wiener_pars(alpha,tau,beta,delta) ||
-      !is.numeric(q) || !(is.character(resp) || is.factor(resp))) {
-    stop("bad parameter values!")
-  }
+  check_wiener_pars(alpha,tau,beta,delta) 
+  stopifnot(is.numeric(q) , (is.character(resp) || is.factor(resp)))
 
   if (!(length(resp) == length(q))) {
     stop("argument q and resp need to be of the same length!")
@@ -62,25 +51,23 @@ pwiener <- function(q, alpha,tau,beta,delta, resp="upper")
   for (i in 1:length(q)) {
     if (q[i]<0) stop("q must be > 0!")
 
-    if (resp[i] == "upper") 
-      p[i] <- .Call(pwiener_c, q[i], alpha,tau,beta,delta)
-    else if (resp[i] == "lower")
-      p[i] <- .Call(pwiener_c, -q[i], alpha,tau,beta,delta)
-    else if (resp[i] == "both")
-      p[i] <- .Call(pwiener_full_c, q[i], alpha,tau,beta,delta)
-    else stop("resp must be either 'lower', 'upper' or 'both'")
-    if(is.nan(p[i])) p[i] <- 0
-  }
+      if (resp[i] == "upper") 
+        p[i] <- .Call(pwiener_c, q[i], alpha,tau,beta,delta)
+      else if (resp[i] == "lower")
+        p[i] <- .Call(pwiener_c, -q[i], alpha,tau,beta,delta)
+      else if (resp[i] == "both")
+        p[i] <- .Call(pwiener_full_c, q[i], alpha,tau,beta,delta)
+      else stop("resp must be either 'lower', 'upper' or 'both'")
+      if(is.nan(p[i])) p[i] <- 0
+    }
 
-  return(p)
-}
+    return(p)
+  }
 
 qwiener <- function(p, alpha,tau,beta,delta, resp="upper")
 {
-  if (!check_wiener_pars(alpha,tau,beta,delta) ||
-      !is.numeric(p) || !(is.character(resp) || is.factor(resp))) {
-    stop("bad parameter values!")
-  }
+  check_wiener_pars(alpha,tau,beta,delta) 
+  stopifnot(is.numeric(p) , (is.character(resp) || is.factor(resp))) 
 
   if (!(length(resp) == length(p))) {
     stop("argument p and resp need to be of the same length!")
@@ -95,32 +82,29 @@ qwiener <- function(p, alpha,tau,beta,delta, resp="upper")
   for (i in 1:length(p)) {
     if (p[i]<0) stop("p must be > 0!")
 
-    if (resp[i] == "upper")
-      q[i] <- .Call(qwiener_c, p[i], alpha,tau,beta,delta)
-    else if (resp[i] == "lower")
-      q[i] <- .Call(qwiener_c, -p[i], alpha,tau,beta,delta)
-    else if (resp[i] == "both")
-      q[i] <- .Call(qwiener_full_c, p[i], alpha,tau,beta,delta)
-    else stop("resp must be either 'lower', 'upper' or 'both'")
-    if(is.nan(q[i])) p[i] <- 0
-  }
+      if (resp[i] == "upper")
+        q[i] <- .Call(qwiener_c, p[i], alpha,tau,beta,delta)
+      else if (resp[i] == "lower")
+        q[i] <- .Call(qwiener_c, -p[i], alpha,tau,beta,delta)
+      else if (resp[i] == "both")
+        q[i] <- .Call(qwiener_full_c, p[i], alpha,tau,beta,delta)
+      else stop("resp must be either 'lower', 'upper' or 'both'")
+      if(is.nan(q[i])) p[i] <- 0
+    }
 
-  return(q)
-}
+    return(q)
+  }
 
 rwiener <- function(n, alpha,tau,beta,delta)
 {
-  if (!check_wiener_pars(alpha,tau,beta,delta)) {
-    stop("bad parameter values!")
-  }
-
+  check_wiener_pars(alpha,tau,beta,delta)
   rdat <- data.frame(q=vector("double"),resp=factor(levels=c("upper", "lower")))
 
   for (i in 1:n) {
     r <- .Call(rwiener_c, alpha,tau,beta,delta)
     if (r >= 0) rdat[i,] <- c(r,"upper")
-    else rdat[i,] <- c(abs(r),"lower")
-    
+      else rdat[i,] <- c(abs(r),"lower")
+
   }
 
   rdat[,1] <- as.double(rdat[,1])
@@ -128,17 +112,15 @@ rwiener <- function(n, alpha,tau,beta,delta)
 }
 
 wiener_likelihood <- function(x, dat) {
- if (!check_wiener_pars(x[1],x[2],x[3],x[4])) {
-    return(-Inf)
-  }
-  ll <- vector("double", length(dat[,1]))
-  for (i in 1:length(dat[,1])) {
-    ll[i] <- dwiener(as.double(dat[i,1]), x[1],x[2],x[3],x[4], 
-                  resp=as.character(dat[i,2]), give_log=TRUE)
+ check_wiener_pars(x[1],x[2],x[3],x[4])
+ ll <- vector("double", length(dat[,1]))
+ for (i in 1:length(dat[,1])) {
+  ll[i] <- dwiener(as.double(dat[i,1]), x[1],x[2],x[3],x[4], 
+    resp=as.character(dat[i,2]), give_log=TRUE)
   }
   return(sum(ll))
 }
-  
+
 wiener_deviance <- function(x, dat) {
   -2*wiener_likelihood(x,dat)
 }
@@ -180,11 +162,11 @@ wiener_plot = function(dat)  {
   par(mar=c(0,5,0,0),mfcol=c(2,1),ask=FALSE)
 
   plot(dpos,xlim=c(0,maxt),ylim=c(0,maxd),las=2,lwd=2,col="green3",
-      main="",ylab="",ask=FALSE)
-      rug(rt[rc== 1],col="green3")
-      mtext("Density of positive responses",side=2,line=4,cex=0.8)
+    main="",ylab="",ask=FALSE)
+  rug(rt[rc== 1],col="green3")
+  mtext("Density of positive responses",side=2,line=4,cex=0.8)
   plot(dneg,xlim=c(0,maxt),ylim=c(maxd,0),las=2,lwd=2,col="red",
-      main="",ylab="",ask=FALSE)
-      mtext("Density of negative responses",side=2,line=4,cex=0.8)
-      rug(rt[rc==2],col="red",side=3)
+    main="",ylab="",ask=FALSE)
+  mtext("Density of negative responses",side=2,line=4,cex=0.8)
+  rug(rt[rc==2],col="red",side=3)
 }
